0c86afbf45627115e69c7ed9319bc8cbceceecaf
caiiru <caiogome125@gmail.com>
2025-04-16 21:45:37 -0300

    FEAT: Implement SIMPLEJSON & RESTAPI
    Simple JSON é uma classe de suporte para JSON na unity.
    A classe RESTAPI foi um teste puxando do banco de dados(Local) uma informação de um quiz e imprimindo na tela da Scene "Caiiru" o nome do quiz.
    A URL pode ser transcrita com um .TXT
    
    Informações sobre o padrão estarão no discord.

---
+++ P_PucQuiz/Assets/Scripts/Tradutor/RestAPI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+
+namespace Tradutor
+{
+    public class RestAPI:MonoBehaviour
+    {
+        private string URL = "http://localhost:8080/api/quizzes?userID=1";
+
+        public int index;
+
+        public TMPro.TextMeshProUGUI quizTitle;
+
+        public bool getData = false;
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            if (getData)
+            {
+                getData = false;
+                StartCoroutine(GetData());
+            }
+        }
+        
+        
+        // ReSharper disable Unity.PerformanceAnalysis
+        IEnumerator GetData()
+        {
+            using (UnityWebRequest request = UnityWebRequest.Get(URL))
+            {
+                yield return request.SendWebRequest();
+
+                if (request.result == UnityWebRequest.Result.ConnectionError)
+                {
+                    Debug.LogError(request.result);
+                }
+                else
+                {
+                    string json = request.downloadHandler.text;
+                    SimpleJSON.JSONNode quizInfo = SimpleJSON.JSON.Parse(json);
+
+                    quizTitle.text = quizInfo[index]["title"];
+                    /*
+                    Quiz q = new Quiz();
+                    q.title=quizInfo[index]["title"];
+                    */
+                }
+            }
+        }
+    }
+}
