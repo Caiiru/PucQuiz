3bfd35f691687641bac62083f68f69c6b2838407
Marco Aurelio Guedes Junior <marco.guedes.jr@gmail.com>
2025-04-22 20:46:46 -0300

    Escrever Dps
    Escrever dps

--- P_PucQuiz/Assets/Scripts/ModosDeJogo/Basico.cs
+++ P_PucQuiz/Assets/Scripts/ModosDeJogo/Basico.cs
@@ -1,6 +1,27 @@
+using UnityEditor.Timeline;
 using UnityEngine;
 
-public class Basico : Modos
+public class Basico : MonoBehaviour
 {
-    
+    public Modos modo;
+
+    public void Awake()
+    {
+        if(Modos.get != null)
+        {
+            modo = Modos.get;
+        }
+        modo.Set_Transform(transform);
+    }
+
+    public void Start()
+    {
+        modo.Awake();
+        modo.Start();
+    }
+
+    public void Update()
+    {
+        modo.Update();
+    }
 }
