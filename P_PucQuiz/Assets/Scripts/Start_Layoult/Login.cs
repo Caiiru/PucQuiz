ed786f53049e57ab0490cc0a4c46ccb3823d6b02
Marco Aurelio Guedes Junior <marco.guedes.jr@gmail.com>
2025-05-01 17:29:11 -0300

    Escrever bonitinho dps
    Escrever bonitinho dps

--- P_PucQuiz/Assets/Scripts/Start_Layoult/Login.cs
+++ P_PucQuiz/Assets/Scripts/Start_Layoult/Login.cs
@@ -1,48 +1,169 @@
-using Mono.Cecil.Cil;
 using System;
-using UnityEditor.SearchService;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class Login : MonoBehaviour
 {
-    public GameObject login, codeORcreate;
+    public DictionaryThree<String, GameObject, VisualTreeAsset>[] menu;
+    public UIDocument doc;
 
     public string layout_actualy;
 
-    void Start()
+    private void Awake()
     {
         Modos.get = null;
-        layout_actualy = "login";
-        login.SetActive(true);
-        codeORcreate.SetActive(false);
+        layout_actualy = "Start";
+    }
+
+    void Start()
+    {
+        ChangeMenu("Start");
     }
 
     void Update()
     {
 
-        if (layout_actualy == "login" && Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("Login = Sucesso");
-            layout_actualy = "createORcode";
-            codeORcreate.SetActive(true);
-            login.SetActive(false);
-        }
-        else
-        {
-            if (layout_actualy == "createORcode" && Input.GetKeyDown(KeyCode.Space))
-            {
-                Debug.Log("Create Game = Sucesso");
-                Event_PucQuiz.Change_Scene(Config_PucQuiz.Get_Config().Layout_Game);
-            }
-        }
+    }
 
-        if (layout_actualy == "createORcode" && (Input.GetKeyDown(KeyCode.RightShift) || Input.GetKeyDown(KeyCode.LeftShift)))
+    #region # Click Events #
+    private void ClickStart(ClickEvent evt)
+    {
+        Debug.Log("Start = Sucesso");
+
+        //menu[0].getValue2().SetActive(false);
+        //menu[1].getValue2().SetActive(true);
+        ChangeMenu("Login");
+    }
+
+    private void ClickLogin(ClickEvent click)
+    {
+        try
         {
-            Debug.Log("Enter Game = Sucesso");
-            Event_PucQuiz.Change_Scene(Config_PucQuiz.Get_Config().Layout_Game);
+            Debug.Log("Email = " + doc.rootVisualElement.Q<TextField>("Email").text);
+            Debug.Log("Senha = " + doc.rootVisualElement.Q<TextField>("Senha").text);
+            Debug.Log("Login = Sucesso");
+
+            layout_actualy = "CreateOrCode";
+        }
+        catch
+        {
+            Debug.Log("Erro ao se logar.");
         }
     }
 
+    private void ChangeMenu(string menu_new)
+    {
+        if (menu_new == null) { Debug.Log("Não foi atribuido um valor ao novo menu buscado."); return; }
 
+        layout_actualy = menu_new;
+
+        for (int i = 0; i < menu.Length; i++)
+        {
+            try
+            {
+                if (menu[i].getValue1() == menu_new)
+                {
+                    menu[i].getValue2().SetActive(true);
+                    doc.visualTreeAsset = menu[i].getValue3();
+                }
+                else
+                {
+                    menu[i].getValue2().SetActive(false);
+                }
+            }
+            catch (Exception error)
+            {
+
+            }
+        }
+
+        for (int i = 0; i < menu.Length; i++)
+        {
+            try
+            {
+
+                if (menu[i].getValue1() == layout_actualy)
+                {
+                    switch (menu[i].getValue1())
+                    {
+                        case "Start":
+                            doc.rootVisualElement.Q<Button>("Play").RegisterCallback<ClickEvent>(ClickStart);
+                            break;
+                        case "Login":
+                            doc.rootVisualElement.Q<Button>("Login").RegisterCallback<ClickEvent>(ClickLogin);
+                            break;
+                        case "CreateOrCode":
+                            break;
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                Debug.Log(error);
+            }
+        }
+    }
+    #endregion
+
+    private void OnDisable()
+    {
+        for (int i = 0; i < menu.Length; i++)
+        {
+            try
+            {
+                switch (menu[i].getValue1())
+                {
+                    case "Start":
+                        doc.rootVisualElement.Q<Button>("Start").UnregisterCallback<ClickEvent>(ClickStart);
+                        break;
+                    case "Login":
+                        doc.rootVisualElement.Q<Button>("Login").UnregisterCallback<ClickEvent>(ClickLogin);
+                        break;
+                    case "CreateOrCode":
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+        }
+    }
+}
+
+[Serializable]
+public class DictionaryThree<Tvalue1, Tvalue2, Tvalue3>
+{
+    [SerializeField] private Tvalue1 value1;
+    [SerializeField] private Tvalue2 value2;
+    [SerializeField] private Tvalue3 value3;
+
+    /*
+    public Type typeKey()
+    {
+        return key.GetType();
+    }
+    public Type type1()
+    {
+        return value1.GetType();
+    }
+    public Type type2()
+    {
+        return value2.GetType();
+    }
+    */
+
+    public Tvalue1 getValue1()
+    {
+        return value1;
+    }
+    public Tvalue2 getValue2()
+    {
+        return value2;
+    }
+
+    public Tvalue3 getValue3()
+    {
+        return value3;
+    }
 }
