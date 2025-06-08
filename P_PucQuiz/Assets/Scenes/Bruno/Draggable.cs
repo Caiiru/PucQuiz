8d12ebd36aa1efac8a578ec6e9e3db4fdd3279ba
Ribeirou1766 <bruno.ribeiro1766@gmail.com>
2025-06-07 21:51:15 -0300

    Card trails and burst click.
    - Card Trail particles
    - Burst click particles
    - Simple drag behavior
    - Simple click particule spawner

---
+++ P_PucQuiz/Assets/Scenes/Bruno/Draggable.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector3 offset;
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void OnMouseDown()
+    {
+        // Calculate offset between mouse and object position
+        Vector3 mouseWorldPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        offset = transform.position - new Vector3(mouseWorldPos.x, mouseWorldPos.y, transform.position.z);
+        isDragging = true;
+    }
+
+    void OnMouseDrag()
+    {
+        if (isDragging)
+        {
+            Vector3 mouseWorldPos = cam.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(mouseWorldPos.x, mouseWorldPos.y, transform.position.z) + offset;
+        }
+    }
+
+    void OnMouseUp()
+    {
+        isDragging = false;
+    }
+}
